a1c24f1556ee19bbc030875a325b03e6170e72e2
Erik Benito <ErikBenito_2017@outlook.com>
2021-10-28 18:40:35 -0300

    Config

--- src/index.js
+++ src/index.js
@@ -473,7 +473,7 @@ app.get("/BuscarUsu/:id", async(req, resp) =>{
 app.get("/localCidade", async (req,resp) => {
 
   try { 
-        const data = await db.infoc_ntc_local.findAll({
+   const data = await db.infoc_ntc_local.findAll({
        group: [ col('infoc_ntc_local.ds_cidade')],
        attributes: [ "ds_cidade",
        [fn('count', "ds_cidade" ), 'qtd']
@@ -510,11 +510,13 @@ app.get("/localBairro", async (req,resp) => {
 
     try{
         const data = await db.infoc_ntc_denuncia.findAll({
-            group: [ col('infoc_ntc_denuncia.dt_depoimento')],
-            attributes: [ "dt_depoimento",
-            [(data.getMonth("dt_depoimento"))]
-            [fn('count', Month, "dt_depoimento" ), 'qtd']
-        ]
+            group:[
+              [fn('date_format',col('dt_cadastro'), '%m')]
+            ],
+            attributes: [
+              [fn('date_format',col('dt_cadastro'), '%m'), 'mes'],
+              [fn('count', 'dt_cadastro'), 'qtd']
+            ]
         })
 
         resp.send(data);
