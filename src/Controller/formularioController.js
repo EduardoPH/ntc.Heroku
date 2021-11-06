af237d39e366f8e604f19df4fb895749360bae7c
Erik Benito <87447975+Erik-Benito@users.noreply.github.com>
2021-11-06 09:41:56 -0300

    Update formularioController.js

--- src/Controller/formularioController.js
+++ src/Controller/formularioController.js
@@ -44,7 +44,7 @@ app.post('/', async (req, resp) => {
             id_vestimento: vestimentasCriada.id_vestimento
         })
 
-        resp.send({mensagem: 'Denuncia cadastrada'})
+        resp.send({mensagem: 'Denúncia cadastrada'})
     } catch (e) {
         resp.send({erro: 'Houve um erro durante o cadastro, todos os campos devem ser preenchidos!'})
     }
@@ -59,21 +59,21 @@ app.put('/', async (req, resp) => {
         let vestimentas = dados.vestimenta
         let local = dados.local
 
-        const vestimentasCriada = await db.infoc_ntc_vestimento.create({
+        const vestimentasCriada = await db.infoc_ntc_vestimento.update({
             ds_inferior: vestimentas.parteBaixo,
             ds_superior: vestimentas.parteCima,
             ds_calcado: vestimentas.calcado,
             ds_complemento: vestimentas.complemento
         })
 
-        const caracteristicasCriada = await db.infoc_ntc_caracteristica_fisica.create({
+        const caracteristicasCriada = await db.infoc_ntc_caracteristica_fisica.update({
             ds_pele: caracteristicas.pele,
             ds_cabelo: caracteristicas.cabelo,
             ds_cor_cabelo: caracteristicas.corCabelo,
             ds_complemento: caracteristicas.complemento
         })
 
-        const localCriada = await db.infoc_ntc_local.create({
+        const localCriada = await db.infoc_ntc_local.update({
             ds_latitude: local.lat,
             ds_longitude: local.lgn,
             ds_bairro: local.bairro,
@@ -89,10 +89,13 @@ app.put('/', async (req, resp) => {
             id_fisico: caracteristicasCriada.id_fisico,
             id_vestimento: vestimentasCriada.id_vestimento
         }, {
-            where: {'id_denuncia': dados.id}
+            where: {'id_denuncia': dados.id,
+                    'id_local': dados.id_local,
+                    'id_fisico': dados.id_fisico,
+                    'id_vestimento': dados.id_vestimento}
         })
 
-        resp.send({mensagem: 'Denuncia cadastrada'})
+        resp.send({mensagem: 'Denúncia cadastrada'})
     } catch (e) {
         resp.send({erro: 'Houve um erro durante o cadastro, todos os campos devem ser preenchidos!'})
     }
