07a8e83ad532e661c2d332fba3c1ba90678f50a2
Erik Benito <87447975+Erik-Benito@users.noreply.github.com>
2021-11-06 12:58:27 -0300

    Update formularioController.js

--- src/Controller/formularioController.js
+++ src/Controller/formularioController.js
@@ -56,48 +56,50 @@ app.put('/', async (req, resp) => {
 
         let usuario = dados.usu
         let caracteristicas = dados.caracteristicas
-        let vestimentas = dados.vestimenta
+        let vestimenta = dados.vestimenta
         let local = dados.local
 
-        const vestimentasCriada = await db.infoc_ntc_vestimento.update({
-            ds_inferior: vestimentas.parteBaixo,
-            ds_superior: vestimentas.parteCima,
-            ds_calcado: vestimentas.calcado,
-            ds_complemento: vestimentas.complemento
+        const vestimentaUpdate = await db.infoc_ntc_vestimento.update({
+            'ds_inferior': vestimenta.parteBaixo,
+            'ds_superior': vestimenta.parteCima,
+            'ds_calcado': vestimenta.calcado,
+            'ds_complemento': vestimenta.complemento
+        },{
+            where: {'id_vestimento': vestimenta.id}
         })
 
-        const caracteristicasCriada = await db.infoc_ntc_caracteristica_fisica.update({
-            ds_pele: caracteristicas.pele,
-            ds_cabelo: caracteristicas.cabelo,
-            ds_cor_cabelo: caracteristicas.corCabelo,
-            ds_complemento: caracteristicas.complemento
+        const caracteristicasUpdate = await db.infoc_ntc_caracteristica_fisica.update({
+            'ds_pele': caracteristicas.pele,
+            'ds_cabelo': caracteristicas.cabelo,
+            'ds_cor_cabelo': caracteristicas.corCabelo,
+            'ds_complemento': caracteristicas.complemento
+        },{
+            where: {'id_fisico': caracteristicas.id}
         })
 
-        const localCriada = await db.infoc_ntc_local.update({
-            ds_latitude: local.lat,
-            ds_longitude: local.lgn,
-            ds_bairro: local.bairro,
-            ds_cidade: local.cidade
+        const localUpdate = await db.infoc_ntc_local.update({
+            'ds_latitude': local.lat,
+            'ds_longitude': local.lgn,
+            'ds_bairro': local.bairro,
+            'ds_cidade': local.cidade
+        },{
+            where: {'id_local': local.id}
         })
 
-        const denunciaCriada = await db.infoc_ntc_denuncia.update({
-            id_usuario: usuario,
-            ds_depoimento: dados.depoimento,
-            dt_cadastro: new Date(),
-            id_local: localCriada.id_local,
-            bt_ativo: false,
-            id_fisico: caracteristicasCriada.id_fisico,
-            id_vestimento: vestimentasCriada.id_vestimento
+        const denunciaUpdate = await db.infoc_ntc_denuncia.update({
+            'id_usuario': usuario,
+            'ds_depoimento': dados.depoimento,
+            'dt_cadastro': new Date(),
+            'id_local': localUpdate.id_local,
+            'bt_ativo': false,
+            'id_fisico': caracteristicasUpdate.id_fisico,
+            'id_vestimento': vestimentaUpdate.id_vestimento
         }, {
-            where: {'id_denuncia': dados.id,
-                    'id_local': dados.id_local,
-                    'id_fisico': dados.id_fisico,
-                    'id_vestimento': dados.id_vestimento}
+            where: {'id_denuncia': dados.id }
         })
-
-        resp.send({mensagem: 'Denúncia cadastrada'})
+        resp.send({mensagem: 'Denúncia Alterada'})
     } catch (e) {
-        resp.send({erro: 'Houve um erro durante o cadastro, todos os campos devem ser preenchidos!'})
+        resp.send({erro: 'Todos os campos devem ser preenchidos'})
     }
 })
 
