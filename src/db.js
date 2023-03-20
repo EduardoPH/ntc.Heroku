117bb47a2834f65afd9ba683319dcd01ed5c34d9
EduardoPH <dudu19424irma@gmail.com>
2023-03-20 02:13:04 -0300

    Merge changes from upstream

--- src/db.js
+++ src/db.js
@@ -1,12 +1,15 @@
 import initdb from './models/init-models.js'
 import Sequelize from 'sequelize';
+import mysql2 from 'mysql2'
 const sequelize = new Sequelize(
 'mysql_17753_nsftcc',
 'nsftcc',
 'nsf@tcc', {
 host: 'my01.winhost.com',
 dialect: 'mysql',
-logging: false
+logging: false,
+dialectModule: mysql2
 });
 const db = initdb(sequelize);
+
 export default db;
